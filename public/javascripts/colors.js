22dc1ab81e32ff00cd4df90b6c826ee106396c52
tommyhu234 <tommyhu234@gmail.com>
2021-12-24 12:09:54 +0100

    Enter

---
+++ public/javascripts/colors.js
@@ -0,0 +1,55 @@
+
+const USED = -1;
+const AVAIL = 1;
+
+function Colors() {
+    this.colors = undefined;
+
+    this.initialize = function() {
+        this.colors = {
+            White: AVAIL,
+            Black: AVAIL,
+            Orange: AVAIL,
+            Brown: AVAIL,
+            Yellow: AVAIL,
+            Blue: AVAIL,
+            Green: AVAIL,
+            Red: AVAIL
+        };
+    };
+
+    this.isColor = function (color) {
+        return Object.prototype.hasOwnProperty(this.colors, color);
+    };
+
+    this.isColorAvailable = function (letter) {
+        return this.isColor(color) && this.colors[color] == AVAIL;
+    };
+
+    this.makeColorUnAvailable = function (color) {
+        if(this.isColor(color)) {
+            this.colors[colors] = USED;
+
+        }
+    };
+
+    this.isColorIn = function(color, code) {
+        if(!this.isColor || !this,this.isColorAvailable) {
+            return false;
+        }
+        return code.indexOf(collor) >= 0;
+    }
+
+    this.getColorIndex = function (color, code) {
+        if(!this.isColorIn(color, code)) {
+            console.log("Color ${color} is not in target code ${code}!")
+            return null;
+        }
+        for(let i = 0; i < code.length; i++) {
+            if(code.charAt(i) == color) {
+                return i;
+            }
+        }
+        return null;
+    }
+}
