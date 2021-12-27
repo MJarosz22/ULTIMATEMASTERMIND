8c3006aaf8862822b1ba67910e2471a493f656e1
tommyhu234 <tommyhu234@gmail.com>
2021-12-27 19:06:09 +0100

    Enter

---
+++ public/javascripts/config.js
@@ -0,0 +1,4 @@
+(function (exports) {
+    exports.MAX_ALLOWED_GUESSES = 12; /* Maximum number of guesses */
+    exports.WEB_SOCKET_URL = "ws://localhost:3000"; /* WebSocket URL */
+  })(typeof exports === "undefined" ? (this.Setup = {}) : exports);
---
+++ public/javascripts/statuses.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+//@ts-check
+
+/**
+ * Object containing all status messages.
+ */
+ const Status = {
+    gameWon: "Congratulations! You won! ",
+    gameLost: "Game over. You lost! ",
+    playAgain: "&nbsp;<a href='/play'>Play again!</a>",
+    player1Intro: "Player 1. Pick the code to guess",
+    prompt: "Code to guess",
+    promptAgainLength: "Try again!",
+    promptChars: "Try again!",
+    promptEnglish: "Try again, it has to be a valid code",
+    chosen: "Your chosen code: ",
+    player2Intro: `Player 2. You win if you can guess the code within ${Setup.MAX_ALLOWED_GUESSES} tries.`,
+    player2IntroNoTargetYet: `Player 2. Waiting for code to guess. You win if you can guess it within ${Setup.MAX_ALLOWED_GUESSES} tries.`,
+    guessed: "Player 2 guessed code ",
+    aborted: "Your gaming partner is no longer available, game aborted. <a href='/play'>Play again!</a>"
+  };
