b11ce5e0b1b2e1e8fe2cc46bce7e5eed1c824ae4
Jialson <marcin.jarosz.022@gmail.com>
2022-01-04 10:02:03 +0100

    game almost done

--- public/javascripts/ui-right.js
+++ public/javascripts/ui-right.js
@@ -1,15 +1,8 @@
-function VisibleCodeBoard() {
-    this.setCode = function(visibleCode) {
-      document.getElementById("code").innerHTML = (Array.isArray(visibleCode) ? visibleCode.join("") : visibleCode);
-    };
-  }
 
-function createColors() {
 
-    const colors = ["purple", "orange", "pink", "red", "darkblue", "yellow", "green", "lightblue"];
-}
 
 function StatusBar() {
   this.setStatus = function(status) {
     document.getElementById("status").innerHTML = status;
-  };
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.sendFile("splash.html", { root: "./public" });
 });
+
+/*Pressing the PLAY button */
+router.get("/play", function(req,res){
+  res.sendFile("game.html",{root: "./public"});
+});
+
 
 module.exports = router;
